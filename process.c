55d3236f1537bb1af528f6447e94052345eb7778
pnuUnixTest <132307858+pnuUnixTest@users.noreply.github.com>
2023-06-02 19:33:07 +0900

    Update process.c

--- process.c
+++ process.c
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include "process.h"
 
-
+/**
+struct Person is for storing persnoal information.
+It has name, phone, and pointer which points to another Person. 
+You have to use this struct. Do not change this definition.
+**/
 typedef struct Person{
      char name[20];
      char phone[20];
@@ -12,7 +16,10 @@ typedef struct Person{
 
 
 Person* Push(Person* head, char N[], char P[]) {
-
+	/**
+	This function creates a new Person and push it to the front of the list. The newly created Person should be the new head of the list
+	Returns: Pointer to the new head of the list
+	**/
 	//todo
 	Person* tmp = NULL;
 
@@ -23,7 +30,12 @@ Person* Push(Person* head, char N[], char P[]) {
  }
 
 void printList(Person* head) {
-	
+	/**
+	This function prints all Person information in the list. Each Person information should be printed in a new line. E.g:
+	---List---
+	Person A	01012345678
+	Person B	02098765432
+	**/
 	Person* curr = head;
 	printf("---List---\n");
 	//todo
@@ -32,7 +44,9 @@ void printList(Person* head) {
 }
 
 void Search(Person* head,char N[]) {
-
+	/**
+	This function find Person with name N in the list and print their infomation. If the Person does not exist, print "Could not find"
+	**/
 	Person* curr = head;
 	int isfind = 1;
 
@@ -48,7 +62,10 @@ void Search(Person* head,char N[]) {
 }
 
 void Delete(Person** head,char N[]) {
-
+	/**
+	This function deletes all Person in the list with name N. The list should stay linked and you should be able to print the list normally.
+	If there is no such Person, you should print "Could not find"
+	**/
 	Person* temp = *head;
 	Person* pre = NULL;
 	//todo
